a10282e0326cbf8cad4dad4b4966c096166131bb
dot-json <zsombor.gazdag@gmail.com>
2022-11-02 13:24:14 +0100

    update

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ import Express from "express";
 import path from "path";
 
 const app = Express();
-const __dirname = path.resolve(path.dirname(""));
+const __dirname = path.resolve();
 
-app.use(Express.static(__dirname + "/site"));
+app.use(Express.static(path.join(__dirname, "site")));
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("yo");
 });
 
 app.get("/light", (req, res) => {
